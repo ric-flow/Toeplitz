f8419bbfc55cd4e558e4cc7d2b9a103a1e399ec0
ric-flow <riccardo_fiori@outlook.it>
2023-04-13 18:22:04 +0200

    create tMatVec

---
+++ functions/tMatVec.m
@@ -0,0 +1,14 @@
+function y = tMatVec(a,b,x)
+
+% prodotto matrice vettore per matrice di Toeplitz
+
+a = a(:);
+b = b(:);
+x = x(:);
+n = length(x);
+
+aux = [a;0;b(n:-1:2)];
+y_aux = ifft(fft(aux).*fft([x;zeros(n,1)]));
+y = y_aux(1:n);
+
+end
