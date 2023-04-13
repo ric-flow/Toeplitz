13f23ddac532e62c82da1a6f7eb7876985fe5371
ric-flow <riccardo_fiori@outlook.it>
2023-04-13 12:21:47 +0200

    update tMatVec2

--- tMatVec2.m
+++ tMatVec2.m
@@ -3,6 +3,11 @@ function y = tMatVec2(fT,x)
 % prodotto matrice vettore per matrice di Toeplitz
 % senza calcolare la fft associata alla matrice 
 % T = toeplitz(a,b), che è passata come parametro fT
+% INPUT:
+% fT : trasformata di Fourier associata alla matrice Toeplitz
+% x : vettore
+% OUTPUT:
+% y = Tx prodotto matrice vettore
 
 n = length(x);
 y_aux = ifft(fT.*fft([x;zeros(n,1)]));
